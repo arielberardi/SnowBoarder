70acbc9e8d1fa67977fa2ab8b4d5fcdfeb11b3a0
Ariel Berardi <aberardi@libertyglobal.com>
2023-07-02 15:02:29 +0100

    Add FinishLine and Died actions

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 3.0f;
+    
+    Rigidbody2D _rigidBody2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        if (_rigidBody2D) 
+        {
+            Debug.LogError("Missing game component: RigidBody2D");
+        }
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            _rigidBody2D.AddTorque(_rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            _rigidBody2D.AddTorque(-_rotationSpeed);
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Ground"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
